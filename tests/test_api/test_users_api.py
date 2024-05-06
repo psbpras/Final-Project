f3dff563d0f54076b42cfd5301a6a6e03ad0b6fc
srivatsan1303 <sj796@njit.edu>
2024-05-05 23:40:54 -0400

    Test cases added successfully

--- tests/test_api/test_users_api.py
+++ tests/test_api/test_users_api.py
@@ -51,6 +51,21 @@ async def test_update_user_email_access_allowed(async_client, admin_user, admin_
     assert response.status_code == 200
     assert response.json()["email"] == updated_data["email"]
 
+@pytest.mark.asyncio
+async def test_update_user_email_access_Not_allowed_test2(async_client, admin_user, verified_user, admin_token):
+    updated_data = {"email": f"updated_{admin_user.id}@example.com"}
+    headers = {"Authorization": f"Bearer {admin_token}"}
+    response = await async_client.put(f"/users/{admin_user.id}", json=updated_data, headers=headers)
+    response = await async_client.put(f"/users/{verified_user.id}", json=updated_data, headers=headers)
+    assert "email already exist" in response.json().get("detail", "")
+
+@pytest.mark.asyncio
+async def test_update_user_email_access_allowed_test3(async_client, admin_user, verified_user, admin_token):
+    updated_data = {"email": f"updated_{admin_user.id}@example.com"}
+    headers = {"Authorization": f"Bearer {admin_token}"}
+    response = await async_client.put(f"/users/{admin_user.id}", json=updated_data, headers=headers)
+    response = await async_client.put(f"/users/{admin_user.id}", json=updated_data, headers=headers)
+    assert response.status_code == 200
 
 @pytest.mark.asyncio
 async def test_delete_user(async_client, admin_user, admin_token):
